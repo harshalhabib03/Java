bc014a0a62db701ad9653a024dcc1c0cb62e2369
harshalhabib03 <harshalhabib2002@gmail.com>
2024-12-17 10:43:32 +0530

    OOP'S

---
+++ 23_OOP'S/AnimalClass.java
@@ -0,0 +1,6 @@
+class AnimalClass{
+	
+	public static void main(String[] args) {
+		System.out.println("This is a class");
+	}
+}
---
+++ 23_OOP'S/AnimalMethod.java
@@ -0,0 +1,10 @@
+class AnimalMethod{
+	String color;
+	int age;
+	public static void main(String[] args) {
+		AnimalMethod am = new AnimalMethod();
+		am.color="Black";
+		am.age=10;
+		System.out.println(am.color+" "+am.age);
+	}
+}
