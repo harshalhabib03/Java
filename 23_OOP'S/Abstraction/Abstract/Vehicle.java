21ffcf2d004f71d96df971acbdb67f12a208c27f
harshalhabib03 <harshalhabib2002@gmail.com>
2024-12-18 10:57:30 +0530

    Abstraction

---
+++ 23_OOP'S/Abstraction/Abstract/Vehicle.java
@@ -0,0 +1,22 @@
+abstract class Vehicle{
+	abstract void start();
+}
+
+class Car extends Vehicle{
+	void start(){
+		System.out.println("car start with key");
+	}
+}
+
+class Scooter extends Vehicle{
+	void start(){
+		System.out.println("scooter start with kick");
+	}
+	public static void main(String[] args) {
+		Car c = new Car();
+		c.start();
+
+		Scooter s = new Scooter();
+		s.start();
+	}
+}
