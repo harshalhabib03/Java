9d566a97398d20fd4e3d1eb351dde2ac465b69ad
harshalhabib03 <harshalhabib2002@gmail.com>
2024-12-24 13:44:07 +0530

    Exception Handling

---
+++ 24_Exception_Handling/Test.java
@@ -0,0 +1,11 @@
+class Test{
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("100/2");
+		System.out.println(100/0);		//arithmeticexception occurs
+		System.out.println("4");
+		System.out.println("5");	
+	}
+}
